64d7890468ded4bcc83e6d7cf606d8797389af82
TaSiya <siyandamakhalima.1@gmail.com>
2018-05-17 14:34:53 +0200

    changed some features

---
+++ js/matching-days.js
@@ -0,0 +1,26 @@
+var days = document.querySelector('.days');
+var date1 = document.querySelector('.date1');
+var date2 = document.querySelector('.date2');
+var daysTemplate = document.querySelector('.daysTemplate').innerHTML;
+
+var dateCompiler = Handlebars.compile(daysTemplate);
+
+document.addEventListener('DOMContentLoaded', function(){
+   var data = {
+      'myList' : [
+         {date : 'Monday'},
+         {date : 'Tuesday'},
+         {date : 'Wednesday'},
+         {date : 'Thursday'},
+         {date : 'Friday'}
+      ]
+   }
+   days.innerHTML = dateCompiler(data);
+});
+
+date1.addEventListener('change', function(){
+
+});
+date2.addEventListener('change', function(){
+
+});
