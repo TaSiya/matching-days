2cf6b50e231295bcb9b43aa157fd8f00c80a46e1
tasiya <siyandamakhalima.1@gmail.com>
2018-05-25 00:19:56 +0200

    beautify the code and also made changes in the colouring but still the tests are not behaving as expected. Still need to change the style in the object

--- js/main-logic.js
+++ js/main-logic.js
@@ -1,71 +1,109 @@
-function MatchingDays(){
-  var weekDays = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  var date1, date2 ;
+function MatchingDays() {
+  var weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var date1, date2;
 
-  var weekdaysData = { weekdays: [
-      {label: "Monday", value: "monday", style: "normal"},
-      {label: "Tuesday", value: "tuesday", style: "normal"},
-      {label: "Wednesday", value: "wednesday", style: "normal"},
-      {label: "Thursday", value: "thursday", style: "normal"},
-      {label: "Friday", value: "friday", style: "normal"},
-      {label: "Saturday", value: "saturday", style: "normal"},
-      {label: "Sunday", value: "sunday", style: "normal"}
-    ]};
+  var weekdaysData = {
+    weekdays: [{
+        label: "Monday",
+        value: "monday",
+        style: "normal"
+      },
+      {
+        label: "Tuesday",
+        value: "tuesday",
+        style: "normal"
+      },
+      {
+        label: "Wednesday",
+        value: "wednesday",
+        style: "normal"
+      },
+      {
+        label: "Thursday",
+        value: "thursday",
+        style: "normal"
+      },
+      {
+        label: "Friday",
+        value: "friday",
+        style: "normal"
+      },
+      {
+        label: "Saturday",
+        value: "saturday",
+        style: "normal"
+      },
+      {
+        label: "Sunday",
+        value: "sunday",
+        style: "normal"
+      }
+    ]
+  };
 
-  function sameWeekday(dat1, dat2){
-       date1 = new Date(dat1);
-       date2 = new Date(dat2);
+  function sameWeekday(dat1, dat2) {
+    date1 = new Date(dat1);
+    date2 = new Date(dat2);
     return weekDays[date1.getDay()] == weekDays[date2.getDay()];
   };
 
-  function determineColour(){
-     var weekdays = [];
-     var weekdaysDataList = checkWeekdaysData().weekdays;
-     for(var i = 0; i < weekdaysDataList.length ; i++){
-         var temp = weekdaysDataList[i];
-         weekdays.push(Object.create(temp));
-     }
-     var weekdaysData = {weekdays};
+  function determineColour() {
+    var weekdays = [];
+    var weekdaysDataList = checkWeekdaysData().weekdays;
+    for (var i = 0; i < weekdaysDataList.length; i++) {
+      var temp = weekdaysDataList[i];
+      weekdays.push(Object.create(temp));
+    }
+    var weekdaysData = {
+      weekdays
+    };
 
-     if(sameWeekday(date1,date2)){
-        var updatedWeekdaysData = updateDayStyle(weekdaysData, checkDayName(date1), "green");
-        return updatedWeekdaysData;
-     }
-     else{
-        var updateDay1 = updateDayStyle(weekdaysData, checkDayName(date1), "cyan");
-        var updateDay2 = updateDayStyle(updateDay1, checkDayName(date2), "red");
-        return updateDay2;
-     }
- }
+    if (sameWeekday(date1, date2)) {
+      var updatedWeekdaysData = updateDayStyle(weekdaysData, checkDayName(date1), "green");
+      return updatedWeekdaysData;
+    } else {
+      var updateDay1 = updateDayStyle(weekdaysData, checkDayName(date1), "cyan");
+      var updateDay2 = updateDayStyle(updateDay1, checkDayName(date2), "red");
+      return updateDay2;
+    }
+  }
 
-  function checkWeekdays(){
+  function checkWeekdays() {
     return weekDays;
   }
 
-  function checkWeekdaysData(){
+  function checkWeekdaysData() {
     return weekdaysData;
   }
 
-  function checkDayName(date){
+  function checkDayName(date) {
     return weekDays[new Date(date).getDay()];
   }
 
-  function updateDayStyle(weekdaysList, day, style){
+  function updateDayStyle(weekdaysList, day, style) {
     var list = weekdaysList.weekdays;
-     for (var i = 0; i < list.length; i++) {
-        if(list[i].label == day){
-         list[i].style = style;
-       }
-     }
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].label == day) {
+        console.log(list[i].style);
+        list[i].label = day;
+        list[i].style = style;
+        console.log(list[i].style);
+      }
+    }
+    console.log(list);
+    console.log(weekdaysList);
+    weekdaysList.weekdays = list;
+    console.log('another weekdaysList data');
+    console.log(weekdaysList);
     return weekdaysList;
   }
 
   return {
-    isSameWeekday : sameWeekday,
-    getWeekdays : checkWeekdays,
-    getWeekdaysData : checkWeekdaysData,
-    getWeekday : checkDayName,
-    setDayStyle : updateDayStyle,
-    whichColour : determineColour
+    isSameWeekday: sameWeekday,
+    getWeekdays: checkWeekdays,
+    getWeekdaysData: checkWeekdaysData,
+    getWeekday: checkDayName,
+    setDayStyle: updateDayStyle,
+    whichColour: determineColour
   }
 }
--- test/matching-days.test.js
+++ test/matching-days.test.js
@@ -1,27 +1,107 @@
-describe('Matching days Tests', function(){
-   describe('Are dates the same', function(){
-      var match1 = MatchingDays();
-      it('1 April 2017 - 20 May 2088', function(){
-         assert.equal(match1.isSameWeekday('1 April 2017', '20 May 2088'), false);
-      });
-      it('2 May 2018 - 23 May 2018', function(){
-         assert.equal(match1.isSameWeekday('2 May 2018','23 May 2018'), true);
-      });
-      it('1 January 1 - 1 January 3000',function(){
-         assert.equal(match1.isSameWeekday('1 January 1', '1 January 3000'), false);
-      });
-   });
-   describe('check day name', function(){
-      var match2 = MatchingDays();
-      it('2 May 2018', function(){
-         assert.equal(match2.getWeekday('2 May 2018'), 'Wednesday');
-      });
-      it('26 April 2008', function(){
-         assert.equal(match2.getWeekday('26 April 2008'), 'Saturday');
-      });
-      it('5 February 2038', function(){
-         assert.equal(match2.getWeekday('5 February 2038'), 'Friday');
-      });
-   });
-   
+describe('Matching days Tests', function () {
+    describe('Are dates the same', function () {
+        var match1 = MatchingDays();
+        it('1 April 2017 - 20 May 2088', function () {
+            assert.equal(match1.isSameWeekday('1 April 2017', '20 May 2088'), false);
+        });
+        it('2 May 2018 - 23 May 2018', function () {
+            assert.equal(match1.isSameWeekday('2 May 2018', '23 May 2018'), true);
+        });
+        it('1 January 1 - 1 January 3000', function () {
+            assert.equal(match1.isSameWeekday('1 January 1', '1 January 3000'), false);
+        });
+    });
+    describe('check day name', function () {
+        var match2 = MatchingDays();
+        it('2 May 2018', function () {
+            assert.equal(match2.getWeekday('2 May 2018'), 'Wednesday');
+        });
+        it('26 April 2008', function () {
+            assert.equal(match2.getWeekday('26 April 2008'), 'Saturday');
+        });
+        it('5 February 2038', function () {
+            assert.equal(match2.getWeekday('5 February 2038'), 'Friday');
+        });
+    });
+    describe('Are colours matching', function () {
+        var match3 = MatchingDays();
+        it('is colour green same day', function () {
+            var weekdaysData = {
+                weekdays: [{
+                        label: "Monday",
+                        value: "monday",
+                        style: "normal"
+                    },
+                    {
+                        label: "Tuesday",
+                        value: "tuesday",
+                        style: "normal"
+                    },
+                    {
+                        label: "Wednesday",
+                        value: "wednesday",
+                        style: "normal"
+                    },
+                    {
+                        label: "Thursday",
+                        value: "thursday",
+                        style: "normal"
+                    },
+                    {
+                        label: "Friday",
+                        value: "friday",
+                        style: "normal"
+                    },
+                    {
+                        label: "Saturday",
+                        value: "saturday",
+                        style: "normal"
+                    },
+                    {
+                        label: "Sunday",
+                        value: "sunday",
+                        style: "normal"
+                    }
+                ]
+            };
+            assert.deepEqual(match3.setDayStyle(weekdaysData, 0, 'green'), {
+                weekdays: [{
+                        label: "Monday",
+                        value: "monday",
+                        style: "normal"
+                    },
+                    {
+                        label: "Tuesday",
+                        value: "tuesday",
+                        style: "normal"
+                    },
+                    {
+                        label: "Wednesday",
+                        value: "wednesday",
+                        style: "normal"
+                    },
+                    {
+                        label: "Thursday",
+                        value: "thursday",
+                        style: "normal"
+                    },
+                    {
+                        label: "Friday",
+                        value: "friday",
+                        style: "normal"
+                    },
+                    {
+                        label: "Saturday",
+                        value: "saturday",
+                        style: "normal"
+                    },
+                    {
+                        label: "Sunday",
+                        value: "sunday",
+                        style: "normal"
+                    }
+                ]
+            });
+        });
+    });
 });
